69fdce95fea5dba1fcea6835f5526693001e4924
Andres Roman <andresrombus@gmail.com>
2017-10-04 12:04:38 -0500

    Final

--- clase1/TablaMultiplicar/src/tablamultiplicar/TablaMultiplicar.java
+++ clase1/TablaMultiplicar/src/tablamultiplicar/TablaMultiplicar.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tablamultiplicar;
-
+import java.util.Scanner;
 /**
- *
- * @author Salas
+ * Autor: Andres Roman
+ * Fecha: 04/10/2017
  */
 public class TablaMultiplicar {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Scanner para entrada de datos
+        Scanner datos = new Scanner(System.in);
+        // Variables
+        int num=0, i=0, multi=0;
+        // Entrada
+        System.out.println("Ingrese el numero");
+        num=datos.nextInt();
+        // Proceso
+        for (i=1;i<=12;i++){
+            multi=num*i;
+            System.out.println(+num+"x"+i+"="+multi); 
+        }
     }
     
 }
